d2510123008f97a4feae73d71e996882fd67e978
Nwabueze Franklin <nwafrank2017@gmail.com>
2023-02-18 19:49:37 +0100

    Security_configurations :lock: apllied

--- Authentication/settings.py
+++ Authentication/settings.py
@@ -19,7 +19,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nwaframk2017@gmail.com'
-EMAIL_HOST_PASSWORD = 'vovmtirovfplbpkz'
+EMAIL_HOST_PASSWORD = 'generated-app-password'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Quick-start development settings - unsuitable for production
